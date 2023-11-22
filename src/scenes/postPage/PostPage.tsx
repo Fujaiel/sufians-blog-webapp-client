067647965c940ffc6720f1e5dec4fb28e4981070
Fujaiel Rahman <fujaielxx@gmail.com>
2023-11-22 20:21:57 +0530

    post page componnent updated

--- src/scenes/postPage/PostPage.tsx
+++ src/scenes/postPage/PostPage.tsx
@@ -119,8 +119,7 @@ const PostPage = () => {
         </div>
         {/* Similar blogs */}
         <div className=" my-8">
-          <h2 className=" text-center text-xl font-semibold">Similar Blogs</h2>
-
+          <h2 className=" text-center text-xl font-semibold my-4">Similar Blogs</h2>
           <div className=" flex justify-between my-4">
             <SmallCard />
             <SmallCard />
